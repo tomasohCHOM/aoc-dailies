4fe6ff221b5885b7a00f0aacf1ed6601226f6f03
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2023-11-22 15:24:41 -0800

    Testing aoc 2022 dailies with discord webhook

--- main.ts
+++ main.ts
@@ -1,22 +1,39 @@
+import { executeWebhook } from "aoc-dailies/lib/discord/webhook.ts";
 import { getAoCDaily } from "aoc-dailies/lib/aoc/mod.ts";
+import { load } from "./deps.ts";
 
-async function executeWebhook() {
+async function prepareDiscordWebhook() {
+  await load({ export: true });
+  const discordWebhookURL = Deno.env.get("DISCORD_WEBHOOK_URL");
+  if (!discordWebhookURL) throw new Error("Discord Webhook not set");
+
   const date = new Date();
+
   // if (date.getMonth() !== 11) {
   //   // Only execute in December.
   //   return;
   // }
 
+  // For testing, we set the year to (current year - 1) = 2023 - 1 = 2022 data
   const year = date.getFullYear() - 1;
   const day = date.getDate();
-  const daily = await getAoCDaily({ year, day });
-  console.log(daily.title);
-  console.log(daily.description);
+  const { title, description } = await getAoCDaily({ year, day });
+
+  await executeWebhook({
+    url: discordWebhookURL,
+    data: {
+      embeds: [{
+        color: 0x00cc00,
+        title: `Advent of Code ${year} Day ${day}: ${title}`,
+        description: description.substring(0, 49) + "...",
+      }],
+    },
+  });
 }
 
 async function main() {
   // Execute the webhook at midnight EST, or 5:00 AM UTC.
-  await Deno.cron("executeWebhook", "* * * * *", executeWebhook);
+  await Deno.cron("prepareDiscordWebhook", "* * * * *", prepareDiscordWebhook);
 }
 
 if (import.meta.main) {
