97fea81909ea3ece3a12788cdb3e62a4b6cdf596
EthanThatOneKid <31261035+EthanThatOneKid@users.noreply.github.com>
2023-11-22 22:57:10 -0800

    fix nits

--- main.ts
+++ main.ts
@@ -1,44 +1,40 @@
 import { executeWebhook } from "aoc-dailies/lib/discord/webhook.ts";
 import { getAoCDaily } from "aoc-dailies/lib/aoc/mod.ts";
-import { load } from "./deps.ts";
+import { load } from "aoc-dailies/deps.ts";
 
-async function prepareDiscordWebhook() {
-  console.log("Preparing Discord Webhook Execution");
-  await load({ export: true });
-  const discordWebhookURL = Deno.env.get("DISCORD_WEBHOOK_URL");
-  if (!discordWebhookURL) throw new Error("Discord Webhook not set");
-
+// executeDiscordWebhook executes the Discord webhook at 5:00 AM UTC (midnight EST).
+async function executeDiscordWebhook() {
   const date = new Date();
-
-  // if (date.getMonth() !== 11) {
-  //   // Only execute in December.
-  //   console.log("Not December yet :(");
-  //   return;
-  // }
 
   // For testing, we set the year to (current year - 1) = 2023 - 1 = 2022 data
   const year = date.getFullYear() - 1;
   const day = date.getDate();
   const { title, description } = await getAoCDaily({ year, day });
 
+  const discordWebhookURL = Deno.env.get("DISCORD_WEBHOOK_URL")!;
   await executeWebhook({
     url: discordWebhookURL,
     data: {
-      embeds: [{
-        color: 0x00cc00,
-        title: `Advent of Code ${year} Day ${day}: ${title}`,
-        description: description.substring(0, 49) + "...",
-      }],
+      embeds: [
+        {
+          color: 0x00cc00,
+          title: `Advent of Code ${year} Day ${day}: ${title}`,
+          description: description.substring(0, 49) + "…",
+        },
+      ],
     },
   });
 }
 
 async function main() {
-  // Execute the webhook at midnight EST, or 5:00 AM UTC.
-  await Deno.cron("prepareDiscordWebhook", "0 5 * * *", prepareDiscordWebhook);
+  await load({ export: true });
 
-  // Execute the webhook every minute (testing purposes)
-  // await Deno.cron("prepareDiscordWebhook", "* * * * *", prepareDiscordWebhook);
+  // Execute the webhook at midnight EST, or 5:00 AM UTC.
+  await Deno.cron(
+    "executeDiscordWebhook",
+    "0 5 * 11,12 *",
+    executeDiscordWebhook,
+  );
 }
 
 if (import.meta.main) {
