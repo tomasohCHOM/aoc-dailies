8d99823d3335546173a5226797139d9c00265553
Chom <112128328+tomasohCHOM@users.noreply.github.com>
2024-11-29 14:38:37 -0800

    add 5 minute reminder

--- main.ts
+++ main.ts
@@ -4,15 +4,15 @@ import { getAoCDaily } from "aoc-dailies/lib/aoc/mod.ts";
 import { load, retry } from "aoc-dailies/deps.ts";
 import { getRandomMessage } from "aoc-dailies/lib/extra/messages.ts";
 
-// executeDiscordWebhook executes the Discord webhook at 5:00 AM UTC (midnight EST).
-async function executeDiscordWebhook() {
+// Executes the AOC Daily Webhook message via an embed
+async function executeDaily() {
+  const discordWebhookURL = Deno.env.get("DISCORD_WEBHOOK_URL")!;
   const date = new Date();
 
   const year = date.getFullYear();
   const day = date.getDate();
   const { title, description, url } = await getAoCDaily({ year, day });
 
-  const discordWebhookURL = Deno.env.get("DISCORD_WEBHOOK_URL")!;
   await executeWebhook({
     url: discordWebhookURL,
     data: {
@@ -37,19 +37,51 @@ async function executeDiscordWebhook() {
   });
 }
 
-// retryPromise will attempt to execute the callback until success or after 60 seconds.
-async function retryPromise() {
-  await retry(executeDiscordWebhook);
+// Webhook message
+async function executeReminder() {
+  const discordWebhookURL = Deno.env.get("DISCORD_WEBHOOK_URL")!;
+  const ROLE_ID = Deno.env.get("DISCORD_ROLE_ID") ?? "";
+
+  const now = new Date();
+  const utcYear = now.getUTCFullYear();
+  const utcMonth = now.getUTCMonth(); // 0-based
+  const utcDate = now.getUTCDate();
+
+  // 5:00 AM UTC
+  const reminderTime = new Date(Date.UTC(utcYear, utcMonth, utcDate, 5, 0, 0));
+
+  // Generate UNIX timestamp for Discord to render it
+  const unixTimestamp = Math.floor(reminderTime.getTime() / 1000);
+
+  const message =
+    `**<@&${ROLE_ID}> Advent Of Code ${utcYear} Day ${utcDate} release <t:${unixTimestamp}:R>!`;
+
+  await executeWebhook({
+    url: discordWebhookURL,
+    data: { content: message },
+  });
+}
+
+// retryPromise will attempt to execute the callback until success or after 60 seconds
+async function retryPromise(callback: () => Promise<void>) {
+  await retry(callback);
 }
 
 async function main() {
   await load({ export: true });
 
-  // Execute at midnight EST, or 5:00 AM UTC.
+  // Execute daily reminder 5 minutes before 5:00 AM UTC
   await Deno.cron(
-    "Executing Discord webhook via retryPromise",
+    "Execute AOC daily reminder via retryPromise",
+    "55 4 1-25 12 *",
+    async () => await retryPromise(executeReminder),
+  );
+
+  // Execute daily message at 5:00 AM UTC
+  await Deno.cron(
+    "Executing AOC message via retryPromise",
     "0 5 1-25 12 *",
-    retryPromise,
+    async () => await retryPromise(executeDaily),
   );
 }
 
